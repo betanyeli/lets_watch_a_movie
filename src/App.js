14c80e6711e058ae48d0ed401825d86f8f0d823a
betanyeli <betangelii@gmail.com>
2020-08-03 23:41:47 -0400

    fetching api using hooks

--- src/App.js
+++ src/App.js
@@ -2,30 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as ApiManager from "./Services/ApiManager";
+import SearchAppBar from './Components/Navbar/Navbar'
 
 export class App extends React.Component {
-  async componentDidMount() {
-    let query = "Eternal Sunshine of the Spotless Mind"
-    await ApiManager.listMovies(query);
-  }
+  // async componentDidMount() {
+  //   let query = "Eternal Sunshine of the Spotless Mind"
+  //   await ApiManager.listMovies(query);
+  // }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+<SearchAppBar />
       </div>
     );
   }
