c140d19c45658ce31d5f2970567fc41745fc7372
betanyeli <betangelii@gmail.com>
2020-08-04 22:42:26 -0400

    deleting comments

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import "./App.css";
 import SearchAppBar from './Components/Navbar/Navbar'
 
 export class App extends React.Component {
-  // async componentDidMount() {
-  //   let query = "Eternal Sunshine of the Spotless Mind"
-  //   await ApiManager.listMovies(query);
-  // }
 
   render() {
     return (
